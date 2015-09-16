4be835fb7884a7023fa545785f41c6205207e504
TomChambrier <tomw08@gmail.com>
2015-09-16 17:50:30 +0100

    way more concise

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ var exec = require('child_process').exec;
 
 module.exports.execute = execute;
 
-function cb(err, stdout, stderr)
-{
-       if(err !== null){
-           console.log("child process failed with " + stderr);
-       }
-}
+
 
 function execute(command, callback){
-    exec(command, callback).stdout.pipe(process.stdout); //executes child process which runs command and when done calls callback
+    exec(command,callback);
 }
 
-execute('echco h', cb);
+
+
