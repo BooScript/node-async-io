3f33bee265eb60dfb7b998efbe4a0816ab363441
TomChambrier <tomw08@gmail.com>
2015-09-16 13:34:36 +0100

    updates child execute function

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ var exec = require('child_process').exec;
 
 module.exports.execute = execute;
 
-function cb(error, data)
+function cb(error, stdout, stderr)
 {
+   if(error){
+       console.log("child process failed with " + stderr);
+       return error;
+   }
 
-
+console.log(stdout);
 }
+
 function execute(command, callback){ //executes child process which runs command and when done calls callback
     var execChild = exec(command,callback);
-    return execChild;
 }
 
 execute('echo hi', cb);
